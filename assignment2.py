866ffcffb17d5e388b0d5eabe47df5d7aaedc33b
arybadj <aryashbadjatya77@gmail.com>
2024-07-19 17:28:50 +0530

    assignment_up

---
+++ assignment2.py
@@ -0,0 +1,39 @@
+# # Ans1...GitHub is a web-based platform that uses Git, a version control system, 
+# # to help developers manage and track changes in their code. It allows multiple people to 
+# # collaborate on a project, track revisions, and contribute to code from anywhere in the world.
+# #  GitHub offers both free and paid plans, catering to individuals and large organizations alike
+
+## ..............answer2...............
+# $ git add
+# warning: in the working copy of class.ipynb, LF will be replaced by CRLF the n ext Line Git touches It 'os. ipynb", LF will be replaced by CRLF the next
+# warning: in the working copy tine Git touches it touches it
+# of
+# warning: in the working copy of "pyhtoh basic.py", IT will he replaced by CRIT he next time Git touches it
+# TUGLAPTOP-MIEN502 MINGWE4/OneDrive/Desktop/pyhiton prog/hello (min)
+# $ git commit -m "python" [main 18187ab] python
+# files changed. 1288 insertions(+)
+# create mode 100644. basir.py create mode 100644 calculator.py
+# 100644 create node 100644 class. ipynb
+# create mode 100644 os.ipynb create mode 100644 pyhtob basic.py
+# HARI APTOR M1 N58 MINGW64/OneDrive/Desktop/pyhton prog/hello (main)
+# $ git push Enumerating objects: 8, done.
+# Counting objects: 100% (8/8), done. Delta compression using up to 16 threads
+# Delta compressiocts: 100% (1/1). Compressing objects: 100% (7/7), done.
+# writing objects: 100% (7/7), 4.76 KiB 4.6 MiB/s, done Total 7 (delta 7), reused 0 (delta 0), pack-reused 0 (from (0)
+# remote: Resolving deltas: 100% (2/2), done.
+# To github.com: arybadj/hello.git Sffbaeb. 18187ab main-main
+# hpULAPTOR-M1587 MTNG64/OneDrive/Desktop/pyhton prog/hello (main)
+# $ git status On branch main On branch main
+# Your branch is up to date with origin/main'.
+# nothing to commit, working tree clean
+# HOULAPTOP MINOR2V MTNGM64/OneDrive/Desktop/pyhton prog/hello (main)
+
+
+
+
+fruits = ['apple', 'banana', 'cherry', 'date']
+a = fruits[1]
+a2 = a[2:4]
+uppercase_a2 = a2.upper()
+
+print(uppercase_a2)  
